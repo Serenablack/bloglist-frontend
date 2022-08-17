70ef13a6a7be9da711ed3f8ee2a97f4554cdc190
Serena Black <sunsetsin1.sy@gmail.com>
2022-08-17 10:39:48 +0545

    completed ex-5.1, started 5.2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,87 @@
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [message, setMessage] = useState(null);
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+      setMessage(`${user.name} logged in`);
+    } catch (exception) {
+      setMessage("Wrong credentials");
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    }
+  };
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
-  return (
+  const loginForm = () => {
+    return (
+      <div>
+        <h2>log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    );
+  };
+
+  // const blogForm = () => {
+  //   return (
+  //     <form onSubmit={noteAdd}>
+  //       <input onChange={handlesetNote} value={note} />
+  //       <button>Submit</button>
+  //     </form>
+  //   );
+  // };
+
+  return user === null ? (
+    loginForm()
+  ) : (
     <div>
       <h2>blogs</h2>
+      {message}{" "}
+      <button type="logout" onClick={() => loginForm()}>
+        logout
+      </button>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
